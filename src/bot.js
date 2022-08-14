52a3dc40c66f595897474c63942d5f83cbb6fc6b
dert1129 <nathancreger1111@gmail.com>
2022-08-13 23:01:04 -0400

    Added logging

--- src/bot.js
+++ src/bot.js
@@ -3,10 +3,20 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER'] })
 const PREFIX = '$'
+const fs = require('fs');
 
 client.on('ready', () => {
   console.log(`${client.user.username} is logged in.`);
 })
+
+const write = async (data) => {
+  data += "\n";
+  await fs.appendFile('./log.log', data, err => {
+    if(err){
+      console.error(err);
+    }
+  })
+}
 
 client.on('message', (message) => {
   if(message.author.bot) return 
@@ -17,17 +27,18 @@ client.on('message', (message) => {
     .split(/\s+/);
     
     if (CMD_NAME === 'wordle'){
-      if (args.length === 0) {
+      const wordle = args[0];
+      if (wordle === undefined) {
+        write(String(wordle));
         return message.reply("please provide the wordle")
       }
-      const wordle = args[0];
-      console.log(wordle);
       if(wordle){
+        write(String(wordle))
         return message.reply(wordle)
       }
     }
     if (CMD_NAME === 'reactionClear'){
-
+        
       }
   }
 });
